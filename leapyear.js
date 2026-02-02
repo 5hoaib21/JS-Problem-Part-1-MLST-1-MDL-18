cc7201fdc01db333b7c13099e397872a0990345d
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-02-02 07:01:16 +0600

    second commit

---
+++ leapyear.js
@@ -0,0 +1,26 @@
+// simple logic
+function isLeapYear(year) {
+  if (year % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const checkedYear = isLeapYear(2048);
+console.log(checkedYear);
+
+function isLeapYear2(year) {
+  if (year % 100 !== 0 && year % 4 === 0) {
+    return true;
+  } else if (year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const checkedYear1 = isLeapYear2(2100);
+const checkedYear2 = isLeapYear2(2400);
+const checkedYear3 = isLeapYear2(1900);
+const checkedYear4 = isLeapYear2(2052);
+console.log(checkedYear1, checkedYear2, checkedYear3, checkedYear4);
